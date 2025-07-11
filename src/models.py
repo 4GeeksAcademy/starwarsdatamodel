d9c69e096a81ea5ad77fc313d507b954a99de484
jusspian <luisfjuspian@gmail.com>
2025-07-11 16:28:19 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,57 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
+    subscription_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(50))
+    birth_year = db.Column(db.String(20))
+    eye_color = db.Column(db.String(50))
+
+    favorites = db.relationship('Favorite', backref='character', lazy=True)
+
+    def __repr__(self):
+        return f'<Character {self.name}>'
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(100))
+    population = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+
+    favorites = db.relationship('Favorite', backref='planet', lazy=True)
+
+    def __repr__(self):
+        return f'<Planet {self.name}>'
+
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character_id = db.Column(
+        db.Integer, db.ForeignKey('character.id'), nullable=True)
+    planet_id = db.Column(
+        db.Integer, db.ForeignKey('planet.id'), nullable=True)
+
+    def __repr__(self):
+        return f'<Favorite User:{self.user_id}, Char:{self.character_id}, Planet:{self.planet_id}>'
+
